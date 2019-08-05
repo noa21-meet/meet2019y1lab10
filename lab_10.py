98e61952de9d268dc5f5471e56f4badca0886179
noa21-meet <noa21@meet.mit.edu>
2019-08-05 09:47:06 -0400

    Stopping here

---
+++ lab_10.py
@@ -0,0 +1,17 @@
+import turtle
+score = 0
+def score "GoodGuy"():
+    turtle.write("YOUR SCORE IS {}". format(len(stamp_list)- START_LENGTH), align = "right", font = ("Ariel, 16, "normal"))
+    score += 1  
+    turtle.write("GAME OVER! Your score is {}". format(len(stamp_list)- START_LENGTH), align = "right", font = ("Ariel, 18, "normal"))
+    quit()
+                                                                                                                 
+def score "BadGuy"():
+    turtle.write("YOUR SCORE IS {}". format(len(stamp_list)- START_LENGTH), align = "Left", font = ("Ariel, 16, "normal"))
+    score += 1  
+    turtle.write("GAME OVER! Your score is {}". format(len(stamp_list)- START_LENGTH), align = "Left", font = ("Ariel, 18, "normal"))
+    quit()
+
+                                                                                                               
+turtle.mainloop()
+                                                                                                               
